cac126eb82b12544086c99d44ea73d28e4593ecd
jakeelwes <jake.elwes@blueyonder.co.uk>
2015-10-27 17:58:29 -0500

    all working, some problem with workspace setup not code

--- ofApp.cpp
+++ ofApp.cpp
@@ -1,16 +1,15 @@
 #include "ofApp.h"
-//#include "Particle.h"
 
-//--------------------------------------------------------------
 void ofApp::setup(){
     
     cageSize = ofGetHeight()-100;
     
     numNodes = 2000;
     
-    soundReactivityAlpha = 200;
+    soundReactivityAlpha = 100;
     soundReactivityAttract = 4;
     bgBrightness = 60;
+    soundP = false;
     
     
     // Initialize particles
@@ -24,12 +23,13 @@ void ofApp::setup(){
     
     soundSetup();
     guiSetup();
+    
 }
 
-//--------------------------------------------------------------
 void ofApp::update(){
     
-    // collision detection
+    
+    // particle collision detection
     for(std::size_t i = 0; i < numNodes; i++){
         particles[i].update();
         
@@ -45,34 +45,62 @@ void ofApp::update(){
         }
     }
     
+    //attractor noise and collision detection
+    
+    t = ofGetFrameNum() * timeSpeed; // time value for noiseField
+    
+    ofPoint field = noiseField(attractorCenter);
+    float speed = (1 + ofNoise(t, field.x, field.y)) / speedLim;
+    
+    attractorCenter.x += ofLerp(-speed, speed, field.x);
+    attractorCenter.y += ofLerp(-speed, speed, field.y);
+    
+    if(attractorCenter.x > cageSize || attractorCenter.x < 0) {
+        attractorCenter.x = ofGetWidth()/2;
+    }
+    
+    if(attractorCenter.y > cageSize || attractorCenter.y < 0) {
+        attractorCenter.y = ofGetHeight()/2;
+    }
+    
     // normalize volume input
-	scaledVol = ofMap(sqrt(smoothedVol), 0.0, 0.412, 0.0, 1.0, true);
+    if(!soundP){
+        scaledVol = ofMap(sqrt(smoothedVol), 0.0, 0.412, 0.0, 1.0, true);
+    }
+    
+    // for SoundPlayer get levels
+    if (soundP) {
+        float * val = ofSoundGetSpectrum(1);
+        scaledVol = val[0]*10;
+    }
 }
 
-//--------------------------------------------------------------
+
 void ofApp::draw(){
     
     cageSize = ofGetHeight()-100;
-
+    
     ofBackgroundGradient(bgBrightness,bgBrightness-60);
     
     // translate 3d object to center
     ofPushMatrix();
-    ofTranslate(ofGetWidth()/2-cageSize/2,ofGetHeight()/2-cageSize/2,-ofGetHeight()/2);
     
-//    ofRotate(mouseX/2, 0, 1, 0 );
-//    ofRotate(mouseY/2, 1, 0, 0);
-
+    // failed attempt to rotate object (can't find midpoint)
+    //    count = (count + 1) % 360;
+    //    ofRotate(count, 0, 1, 0);
+    
+    ofTranslate(ofGetWidth()/2-cageSize/2,ofGetHeight()/2-cageSize/2, -cageSize);
+    
     float minimumDist = scaledVol * soundReactivityAlpha;
     
     for (std::size_t i = 0; i < particles.size(); i++)
     {
         
         // seeking force (to center), attraction power based on sound input
-        particles[i].seek(ofPoint(cageSize/2,cageSize/2,cageSize/2));
+        particles[i].seek(ofPoint(attractorCenter.x,attractorCenter.y,cageSize/2));
         particles[i].power = scaledVol * soundReactivityAttract;
-
-
+        
+        
         for (std::size_t j = 0; j < i; j++)
         {
             float ppDist = particles[i].position.distance(particles[j].position);
@@ -84,7 +112,7 @@ void ofApp::draw(){
                 if (bgBrightness < 150){
                     col = 255;
                 } else {
-                   col = 0;
+                    col = 0;
                 }
                 ofSetColor(col, alpha);
                 
@@ -93,35 +121,49 @@ void ofApp::draw(){
         }
     }
     
+    
+    
+    
     ofPopMatrix();
 }
 
+// ---------------- NOISE FIELD ----------------
 
-// GUI
+ofPoint ofApp::noiseField(ofPoint position) {
+    float normx = ofNormalize(position.x, 0, ofGetWidth());
+    float normy = ofNormalize(position.y, 0, ofGetHeight());
+    float u = ofNoise(t + phase, normx * jitter + phase, normy * jitter + phase);
+    float v = ofNoise(t - phase, normx * jitter - phase, normy * jitter + phase);
+    return ofPoint(u, v);
+}
+
+// ---------------- GUI ----------------
 
 void ofApp::guiSetup(){
-    gui = new ofxDatGui( ofxDatGuiAnchor::TOP_LEFT );
-    gui->addSlider("Sound Reactivity Alpha", 50, 800);
-    gui->addSlider("Sound Reactivity Attract", -10, 16);
-    gui->addSlider("maxAttractionForce", 0, 0.2);
-//    ofxDatGui2dPad* pad = gui->add2dPad("Attraction Center");
-    gui->addSlider("Background Brightness", 60, 255);
+    gui = new ofxDatGui( ofxDatGuiAnchor::TOP_RIGHT );
+    
+    gui->addSlider("Sound React Alpha", 50, 800);
+    gui->addSlider("Sound Attract", -10, 16);
+    gui->addSlider("max Attract Force", 0, 0.2);
+    gui->addSlider("Brightness", 60, 255);
     gui->addButton("Reset Velocity");
+    gui->addButton("Play Sound");
+    gui->addButton("Stop Sound");
     gui->onSliderEvent(this, &ofApp::onSliderEvent);
-//    gui->on2dPadEvent(this, &ofApp::on2dPadEvent);
     gui->onButtonEvent(this, &ofApp::onButtonEvent);
     gui->setOpacity(0.2);
+    gui->addHeader(":: drag to reposition ::");
     gui->addFooter();
 }
 
 void ofApp::onSliderEvent(ofxDatGuiSliderEvent e){
-    if (e.target->is("Sound Reactivity Alpha")) soundReactivityAlpha = e.target->getValue();
-    if (e.target->is("Sound Reactivity Attract")) soundReactivityAttract = e.target->getValue();
-    if (e.target->is("maxAttractionForce"))
+    if (e.target->is("Sound React Alpha")) soundReactivityAlpha = e.target->getValue();
+    if (e.target->is("Sound Attract")) soundReactivityAttract = e.target->getValue();
+    if (e.target->is("max Attract Force"))
         for(std::size_t i = 0; i < numNodes; i++){
             particles[i].maxForce = e.target->getValue();
         }
-    if (e.target->is("Background Brightness")) bgBrightness = e.target->getValue();
+    if (e.target->is("Brightness")) bgBrightness = e.target->getValue();
     
 }
 
@@ -130,15 +172,11 @@ void ofApp::onButtonEvent(ofxDatGuiButtonEvent e){
         for(std::size_t i = 0; i < numNodes; i++){
             particles[i].velocity = ofPoint(0,0,0);
         }
+    if (e.target->is("Play Sound")) soundPlayer(), soundP = true;
+    if (e.target->is("Stop Sound")) bach.stop(), soundP = false;
 }
 
-//void ofApp::on2dPadEvent(ofxDatGui2dPadEvent e){
-//    attractorCenterx = ofMap(e.x, 9.9, 1014,0,cageSize);
-//    attractorCentery = ofMap(e.y, 14.4, 763.2,0,cageSize);
-//}
-
-
-// SOUND
+// ---------------- SOUND ----------------
 
 void ofApp::soundSetup(){
     
@@ -161,10 +199,8 @@ void ofApp::audioIn(float * input, int bufferSize, int nChannels){
     
     float curVol = 0.0;
     
-    // samples are "interleaved"
     int numCounted = 0;
     
-    //lets go through each sample and calculate the root mean square which is a rough way to calculate volume
     for (int i = 0; i < bufferSize; i++){
         left[i]		= input[i*2]*0.5;
         right[i]	= input[i*2+1]*0.5;
@@ -174,10 +210,8 @@ void ofApp::audioIn(float * input, int bufferSize, int nChannels){
         numCounted+=2;
     }
     
-    //this is how we get the mean of rms :)
     curVol /= (float)numCounted;
     
-    // this is how we get the root of rms :)
     curVol = sqrt( curVol );
     
     smoothedVol *= 0.93;
@@ -187,4 +221,10 @@ void ofApp::audioIn(float * input, int bufferSize, int nChannels){
     
 }
 
+void ofApp::soundPlayer(){
+    
+    bach.load("bach.mp3");
+    bach.play();
+    bach.setLoop(true);
+}
 
--- ofApp.h
+++ ofApp.h
@@ -6,52 +6,68 @@
 
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
     
     // particles
     
+    std::vector<Particle> particles;
     
-        std::vector<Particle> particles;
-		
-        float cageSize, numNodes;
+    float cageSize, numNodes;
+    int count;
     
-
+    
+    // NOISE FIELD (for moving attractor)
+    
+    ofPoint noiseField(ofPoint position);
+    
+    float phase = TWO_PI; // separate u-noise from v-noise
+    float speedLim = 0.8; // particle speed cap
+    float jitter = 3; // wind complexity
+    float timeSpeed = .02; // wind variation speed
+    float t;
+    
+    ofPoint attractorCenter;
+    
     
     // GUI
     
-        void guiSetup();
-        void onSliderEvent(ofxDatGuiSliderEvent e);
-        void onButtonEvent(ofxDatGuiButtonEvent e);
-//        void on2dPadEvent(ofxDatGui2dPadEvent e);
-
+    void guiSetup();
+    void onSliderEvent(ofxDatGuiSliderEvent e);
+    void onButtonEvent(ofxDatGuiButtonEvent e);
+    //        void on2dPadEvent(ofxDatGui2dPadEvent e); // using co-ordinates to move center before noise field
     
-        ofxDatGui* gui;
     
-        float soundReactivityAlpha;
-        float soundReactivityAttract;
-        float bgBrightness;
-//        float attractorCenterx;
-//        float attractorCentery;
+    ofxDatGui* gui;
+    
+    float soundReactivityAlpha;
+    float soundReactivityAttract;
+    float bgBrightness;
+    //        float attractorCenterx;
+    //        float attractorCentery;
     
     
     // SOUND
     
-        void soundSetup();
-        void audioIn(float * input, int bufferSize, int nChannels);
+    void soundSetup();
+    void audioIn(float * input, int bufferSize, int nChannels);
+    void soundPlayer();
     
-        ofSoundStream soundStream;
-
-        std::vector <float> left;
-        std::vector <float> right;
+    ofSoundStream soundStream;
+    ofSoundPlayer bach;
     
-        int bufferCounter;
-        int drawCounter;
+    std::vector <float> left;
+    std::vector <float> right;
     
-        float smoothedVol;
-        float scaledVol;
-
+    int bufferCounter;
+    int drawCounter;
+    bool soundP;
+    
+    float smoothedVol;
+    float scaledVol;
+    
 };
